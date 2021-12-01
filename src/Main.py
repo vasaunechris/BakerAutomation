330df42e3898655977035e5f9a133d33f7fc69bf
Christian Vasaune <chrisngangom69@gmail.com>
2021-12-01 14:07:49 +0100

    modif

--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,10 @@
+from engine.Windows import *
+
+def main():
+    
+    win = Windows()
+
+
+if __name__ == "__main__":
+    main()
 
--- src/engine/Windows.py
+++ src/engine/Windows.py
@@ -1,20 +1,21 @@
 import pygame
 
-class Window:
+class Windows:
 
     def __init__(self, gp):
 
         self.gamePainter = gp
-        background_colour = (255,255,255)
-        (width, height) = (self.gamePainter.getWidth(), self.gamePainter.getHeight())
+        (width, height) = (300,300)#(self.gamePainter.getWidth(), self.gamePainter.getHeight())
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption('Test')
+        background_colour = (255,255,255)
         self.screen.fill(background_colour)
         pygame.display.flip()
-        
 
+    @classmethod
     def paint(self):
-        self.gamePainter.draw()
+        self.gamePainter.drawGame()
+        pass
 
 
     
