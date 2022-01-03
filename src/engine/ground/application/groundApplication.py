6fd1d1b1f711c72239d5dfd28ac65bd5fb148111
Christian Vasaune <chrisngangom69@gmail.com>
2022-01-03 17:38:13 +0100

    init main engine

---
+++ src/engine/ground/application/groundApplication.py
@@ -0,0 +1,16 @@
+class Application:
+
+    def __init__(self):
+        pass
+
+    def run():
+        pass
+
+    def quit():
+        pass
+
+    def onInit():
+        pass
+
+    def onQuit():
+        pass
